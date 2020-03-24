e053a78963e3b912a2bb0186e6aac7378d601cc5
brayan.esc <brayan.esc@outlook.com>
2020-03-24 19:03:56 -0400

    End of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -18,7 +18,8 @@ namespace DatingApp.API.Controllers
         {
             _context = context;
         }
-
+        
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
